296cee2176aff9019f1d8955f6718d6fc6876601
Masakazu Imanishi <macfoce@gmail.com>
2022-05-02 12:51:49 +0900

    - 独自axiosを使うようにする

--- frontend/src/states/entrants.tsx
+++ frontend/src/states/entrants.tsx
@@ -1,14 +1,14 @@
 import {atom, selector} from 'recoil';
-import axios from "axios";
+import axios from "../utils/axios";
 
 export default atom<undefined | null>({
     key: 'entrants',
     default: selector({
         key: 'getEntrants',
         get: async ({get}) => {
-            const CNAME = "karatsu2022";
+            const CNAME = "kumakougen2022";
             try {
-                const res = await axios.get(`http://localhost:8888/api/entrants/?cname=${CNAME}`);
+                const res = await axios.get(`/api/entrants/?cname=${CNAME}`);
                 return res.data.entrants;
             } catch (error) {
                 throw error;
--- frontend/src/states/specialStages.tsx
+++ frontend/src/states/specialStages.tsx
@@ -1,14 +1,14 @@
 import {atom, selector} from 'recoil';
-import axios from "axios";
+import axios from "../utils/axios";
 
 export default atom<undefined | null>({
     key: 'specialStages',
     default: selector({
         key: 'getSpecialStages',
         get: async ({get}) => {
-            const CNAME = "karatsu2022";
+            const CNAME = "kumakougen2022";
             try {
-                const res = await axios.get(`http://localhost:8888/api/specialStages/?cname=${CNAME}`);
+                const res = await axios.get(`/api/specialStages/?cname=${CNAME}`);
                 return res.data;
             } catch (error) {
                 throw error;
